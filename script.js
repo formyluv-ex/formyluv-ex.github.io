084ee8a388f0e1fe89746d8949776d65203587b9
formyluv-ex <revsetiafurniture@gmail.com>
2025-09-29 21:35:32 +0700

    script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Galaxy of love From Latiii',//untuk deskripsi planet layer 1
+  "Luv U <3",//untuk deskripsi planet layer 2
+  "♡Happy Birthday (sayang?)♡",//untuk deskripsi planet layer 3
+  "18/10/2025",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
